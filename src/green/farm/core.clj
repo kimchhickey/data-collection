a4c087002490b2b2b79f9eff3e05e63f282be59c
Sungmin Yang <kimchhickey@gmail.com>
2020-08-31 16:38:05 +0900

    생육정보 저장

--- src/green/farm/core.clj
+++ src/green/farm/core.clj
@@ -27,6 +27,22 @@
     (->> all-user-cropping
          (map f))))
 
+(defn get-cultivate-list [api]
+  "생육정보 목록"
+  (let [startDate "2000-01-01"
+        endDate   "2021-01-01"
+        f (fn [{:keys [userId croppingSerlNo] :as m}]
+            (->> (api userId croppingSerlNo startDate endDate)
+                 (filter :itemCode)
+                 (map #(merge % m))))]
+    (->> all-user-cropping
+         (map f)
+         (flatten))))
+
+(def cultivate-list
+  {:strawberry   (get-cultivate-list api/strawberry-cultivate-list)
+   :korean-melon (get-cultivate-list api/fruit-cultivate-list)
+   :etc          (get-cultivate-list api/cultivate-list)})
 
 (defn map->csv [data columns filename]
   (with-open [writer (io/writer filename)]
@@ -63,8 +79,68 @@
                  :stndSolar
                  :stndTemp
                  :stndWeight]]
-    (map->csv data columns "작기정보목록.csv"))
+    (map->csv data columns "1. 작기정보목록.csv"))
 
+  ;; 1. 농가별 작기 현황 정보
+  ;; 2. 작기 정보 목록
+  ;; 3. 환경 정보 (시설원예)
+  
+  
+  (let [data    (:etc cultivate-list)
+        columns [:itemCode
+                 :userId
+                 :croppingSerlNo
+                 :measDate
+                 :sampleNum
+                 :growLength
+                 :flowerTop
+                 :stemDiameter
+                 :leavesLength
+                 :leavesWidth
+                 :leavesNum
+                 :flowerPosition
+                 :fruitsPosition
+                 :fruitsNum
+                 :harvestPosition
+                 :ped
+                 :solarCorrection
+                 :fruitsWeight]]
+    (map->csv data columns "4.생육정보_토마토_파프리카_오이_가지.csv"))
+  
+  (let [data    (:strawberry cultivate-list)
+        columns [:itemCode
+                 :userId
+                 :croppingSerlNo
+                 :measDate
+                 :sampleNum
+                 ;; 아래 두 필드는 API에서 값이 누락됨.
+                 ;; :growLength
+                 ;; :flowerTop
+                 :leavesNumber
+                 :leavesLength
+                 :petioleLength
+                 :thecaDiameter
+                 :fruitClusterNum
+                 :firstFlowerNum
+                 :secondFlowerNum
+                 :thirdFlowerNum
+                 :firstFruitsNum
+                 :secondFruitsNum
+                 :thirdFruitsNum
+                 :leavesCutWeight
+                 :budding]]
+    (map->csv data columns "5.생육정보_딸기.csv"))
+
+  (let [data    (:korean-melon cultivate-list)
+        columns [:itemCode
+                 :userId
+                 :croppingSerlNo
+                 :measDate
+                 :sampleNum
+                 :leavesLength
+                 :leavesWidth
+                 :thecaDiameter]]
+    (map->csv data columns "6. 생육정보_참외.csv"))
 
   (let [data    all-management
         columns [:userId
@@ -79,7 +155,5 @@
                  :statusCode
                  :statusMessage
                  :preventionPrice]]
-    (map->csv data columns "경영정보목록.csv"))
-
-
+    (map->csv data columns "7. 경영정보목록.csv"))
   )
